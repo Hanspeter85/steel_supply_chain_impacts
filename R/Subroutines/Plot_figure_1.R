40432faa5d925a6096458ef348be57fb7708caa8
Hanspeter85 <hp.wieland@gmx.at>
2024-06-04 11:02:00 +0200

    ...

--- R/Subroutines/Plot_figure_1.R
+++ R/Subroutines/Plot_figure_1.R
@@ -64,8 +64,10 @@ plot_1 <- ggplot() +
   scale_x_discrete(labels = Biome_label$plot,
                    name = "Biome of Iron Ore Extraction") +
   theme(legend.title.position = "top",
-        legend.title.align = 0.5,
-        legend.title = element_text(size = legend_text_size, face = "bold", vjust = 1),
+        legend.title = element_text(size = legend_text_size, 
+                                    face = "bold", 
+                                    vjust = 1,
+                                    hjust = 0.5),
         legend.position = "inside",
         legend.position.inside = c(0.8, 0.7),
         legend.background = element_rect(color = "lightgray",fill = "gray97", linewidth = 0.2),
@@ -75,8 +77,8 @@ plot_1 <- ggplot() +
         axis.title = element_text(size = axis_title_size),
         axis.text = element_text(size = axis_text_size, colour = "black"),
         panel.grid.major.x = element_blank(),
-        panel.border = element_rect(color = "grey", fill = NA, size = 1)) +
-  geom_vline(xintercept = seq(0.5, 7, by = 1), color="gray", size=.5, alpha=.5)
+        panel.border = element_rect(color = "grey", fill = NA, linewidth = 1)) +
+  geom_vline(xintercept = seq(0.5, 7, by = 1), color="gray", linewidth=.5, alpha=.5)
 
 plot_1
 
@@ -99,10 +101,14 @@ plot_2 <- ggplot() +
   geom_bar(data = dat, aes(x = stressor, y = value, fill = source_region_group), 
            stat = "identity", position = "fill") +
   theme_minimal() +
-  scale_fill_manual(values = reg_color) +
-  theme(legend.title = element_blank(),
-        #        legend.position = c(0.7, 0.8),
-        legend.position = "bottom",
+  scale_fill_manual(values = reg_color,
+                    name = "Region of Extraction") +
+  theme(legend.position = "bottom",
+        legend.title.position = "top",
+        legend.title = element_text(size = legend_text_size, 
+                                    face = "bold", 
+                                    vjust = 1,
+                                    hjust = 0.5),
         legend.background = element_rect(color = "lightgray",
                                          fill = "gray97", 
                                          linewidth = 0.2),
@@ -112,13 +118,13 @@ plot_2 <- ggplot() +
         axis.title = element_text(size = axis_title_size),
         axis.text = element_text(size = axis_text_size, color = "black"),
         panel.grid.major.x = element_blank(),
-        panel.border = element_rect(color = "grey", 
-                                    fill = NA, 
-                                    size = 1)) +
+        panel.grid.minor.y = element_blank()) +
   geom_vline(xintercept = seq(0.5, 14, by = 1), color="gray", size=.5, alpha=.5) +
-#  labs(x = "Territorial Pressures and Impacts of Iron Ore Mining") +
-  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
-  scale_x_discrete(labels = x_labels)
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1),
+                     breaks = c(0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1),
+                     expand = c(0,0)) +
+  scale_x_discrete(labels = x_labels,
+                   expand = c(0,0))
 
 plot_2  
 
@@ -133,24 +139,55 @@ dat <- Results_agg %>%
   summarise(value = sum(value), .groups = 'drop') %>% 
   mutate(source_region_group = factor(source_region_group, levels = reg_sort_DE)) %>% 
   pivot_wider(names_from = stressor, values_from = value) %>% 
-  mutate(m2_per_kg = 10^6*eLand/RMC) %>% 
+  mutate(m2_per_t = 10^6*eLand/RMC) %>% 
   mutate(NPP_per_m2 = eHANPP/eLand) %>% 
-  select(m2_per_kg, NPP_per_m2, source_region_group)
+  select(m2_per_t, NPP_per_m2, source_region_group) %>% 
+  mutate(source_region_group = factor(source_region_group, levels = reg_sort_DE))
 
 # calculate global averages
+tot <- Results_agg %>% 
+  filter(source_region_group != "Japan", stressor != "Steel_GAS") %>% 
+  ungroup() %>%  
+  select(stressor, value) %>% 
+  group_by(stressor) %>% 
+  summarise(value = sum(value), .groups = 'drop')
 
+average_m2_per_t <- 10^6*tot$value[tot$stressor == "eLand"]/tot$value[tot$stressor == "RMC"]
+average_NPP_per_m2 <- tot$value[tot$stressor == "eHANPP"]/tot$value[tot$stressor == "eLand"]
 
-ggplot( data = dat, aes(x = NPP_per_m2, y = m2_per_kg, color =  source_region_group) ) +
-  geom_point(alpha = 0.8, size=4) +
+# Select symbols for point shapes of regions
+#reg_symbol <- c(21,21,21,21,22,22,22,22,25,25,25,25)
+reg_symbol <- c(15,15,15,15,19,19,19,19,17,17,17,17)
+
+ggplot( data = dat, aes(x = NPP_per_m2, y = m2_per_t, group =  source_region_group) ) +
+  geom_point(size = 5, aes(shape = source_region_group, color = source_region_group ) ) +
   theme_minimal() +
+  coord_fixed(ratio = (1000/9)) +
+  theme(panel.border = element_rect(color = "grey", 
+                                    fill = NA, 
+                                    size = 0.8),
+        legend.title = element_text(size = legend_text_size, 
+                                    face = "bold", 
+                                    vjust = 1,
+                                    hjust = 0.5),
+        legend.background = element_rect(color = "lightgray",
+                                         fill = "gray97", 
+                                         linewidth = 0.2)) +
+  scale_shape_manual(values = reg_symbol) +
   scale_color_manual(values = reg_color) +
-  scale_x_continuous( limits = c(0, 1000) ) +
-  scale_y_continuous( limits = c(0, 9) )
+  scale_x_continuous( limits = c(0, 1000),
+                      expand = c(0,0)) +
+  scale_y_continuous( limits = c(0, 9),
+                      expand = c(0,0))
 
-  scale_shape_manual(values = world_scatter$symbol ) +
-  scale_color_manual(values = world_scatter$color) +
+  
 
-  labs( x= paste0("",x_lab," [log10 tonnes]"),
+
+
+
+
+
+labs( x= paste0("",x_lab," [log10 tonnes]"),
         y = paste0(y_lab," [log10 tonnes]") ) +
   theme( panel.grid.minor = element_blank(), 
          legend.position = "top",
